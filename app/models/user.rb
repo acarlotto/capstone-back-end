aae65d9f9e460226691072d6957c093c81810e90
acarlotto <acarlotto@cox.net>
2017-10-03 13:19:24 -0400

    changed files to add orders and then removed again

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   include Authentication
   has_many :examples
   has_many :listings, dependent: :destroy
+  # has_many :sales, class_name: "Order", foreign_key: "seller_id"
+  # has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   resources :listings
+  # resources :orders
+
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
